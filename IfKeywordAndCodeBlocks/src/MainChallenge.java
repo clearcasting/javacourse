7d04c022c8a360f92ba2261049ab293286ebf730
unknown <lateralcreativity@outlook.com>
2024-05-28 09:07:52 -0500

    lesson complete

--- IfKeywordAndCodeBlocks/src/MainChallenge.java
+++ IfKeywordAndCodeBlocks/src/MainChallenge.java
@@ -1,39 +1,21 @@
 public class MainChallenge {
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        int highScore = calculateScore(true, 800, 5, 100);
+        System.out.println("The highScore is " + highScore);
 
+        System.out.println("The highScore is " +
+            calculateScore(true, 10000, 8, 200));
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         int finalScore = score;
-        if(gameOver) {
+
+        if (gameOver) {
             finalScore += (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
         }
 
-        // challenge
-//        boolean newGameOver = true;
-//        int newScore = 10000;
-//        int newLevelCompleted = 8;
-//        int newBonus = 200;
-//
-//        int newFinalScore = newScore;
-//
-//        if(newGameOver) {
-//            newFinalScore += (newLevelCompleted * newBonus);
-//            System.out.println("Your final score was " + newFinalScore);
-//        }
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        finalScore = score;
-        if(gameOver) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        return finalScore;
     }
 }
