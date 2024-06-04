08449ffe0e9033be2d6e21bd683f332b3606d44d
unknown <lateralcreativity@outlook.com>
2024-06-04 18:56:11 -0500

    finished next lesson

--- Section 9 Arrays/UsingArrays/src/dev/lpa/Main.java
+++ Section 9 Arrays/UsingArrays/src/dev/lpa/Main.java
@@ -20,6 +20,23 @@ public class Main {
 
         int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
         System.out.println(Arrays.toString(fourthArray));
+
+        String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+
+        if (Arrays.binarySearch(sArray, "Mark") >= 0) {
+            System.out.println("Found Mark in the list");
+        }
+
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {1, 2, 3, 4, 5};
+
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("Arrays are equal");
+        } else {
+            System.out.println("Arrays are not equal");
+        }
     }
 
     private static int[] getRandomArray(int len) {
