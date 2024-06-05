4faac73ba70ba024f41d126439523862ac6d744a
unknown <lateralcreativity@outlook.com>
2024-06-05 15:39:04 -0500

    finished lesson

---
+++ Section 9 Arrays/VarArgs/src/dev/lpa/Main.java
@@ -0,0 +1,25 @@
+package dev.lpa;
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Hello World again");
+        String[] splitStrings = "Hello World again".split(" ");
+        printText(splitStrings);
+
+        System.out.println("_".repeat(20));
+        printText("Hello");
+
+        System.out.println("_".repeat(20));
+        printText("Hello", "World", "again");
+
+        System.out.println("_".repeat(20));
+        String[] sArray = {"first", "second", "third", "fourth", "fifth"};
+        System.out.println(String.join(",", sArray));
+    }
+
+    private static void printText(String... textList) {
+        for (String t: textList) {
+            System.out.println(t);
+        }
+    }
+}
