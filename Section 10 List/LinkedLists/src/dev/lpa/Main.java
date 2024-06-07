618e3f3edbacf2447146c9ec8107d7ceaa57c331
unknown <lateralcreativity@outlook.com>
2024-06-07 14:34:59 -0500

    done

--- Section 10 List/LinkedLists/src/dev/lpa/Main.java
+++ Section 10 List/LinkedLists/src/dev/lpa/Main.java
@@ -22,7 +22,8 @@ public class Main {
 //        gettingElements(placesToVisit);
 //        printItinerary(placesToVisit);
 //        printItinerary2(placesToVisit);
-        printItinerary3(placesToVisit);
+//        printItinerary3(placesToVisit);
+        testIterator(placesToVisit);
     }
 
     public static void addMoreElements(LinkedList<String> list) {
@@ -117,5 +118,22 @@ public class Main {
             previousTown = town;
         }
         System.out.println("Trip ends at " + list.getLast());
+    }
+
+    private static void testIterator(LinkedList<String> list) {
+        var iterator = list.listIterator();
+        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+            if (iterator.next().equals("Brisbane")) {
+                iterator.add("Lake Wivenhoe");
+            }
+        }
+        while (iterator.hasPrevious()) {
+            System.out.println(iterator.previous());
+        }
+        System.out.println(list);
+
+        var iterator2 = list.listIterator(3);
+        System.out.println(iterator2.previous());
     }
 }
