b1de2ef7d37309b38c7e5db99d5fc4b375422467
clearcasting <lateralcreativity@outlook.com>
2024-06-09 10:11:38 -0500

    finished challenge

--- Section 11 Abstraction/InterfaceChallenge/src/dev/lpa/Main.java
+++ Section 11 Abstraction/InterfaceChallenge/src/dev/lpa/Main.java
@@ -2,6 +2,10 @@ package dev.lpa;
 
 public class Main {
     public static void main(String[] args) {
+        Building building = new Building("Sky Scraper", GeometryType.POINT, IconType.RED_STAR, "Sears Tower", Building.Usage.COMMERCIAL);
+        building.toJSON(building);
 
+        UtilityLine utilityLine = new UtilityLine("Line", GeometryType.LINE, IconType.GREEN_DOTTED, "College St", UtilityLine.Utility.FIBER_OPTIC);
+        utilityLine.toJSON(utilityLine);
     }
 }
