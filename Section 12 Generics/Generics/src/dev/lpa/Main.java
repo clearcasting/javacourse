3e7074a898ccaa614b9025981d744ecee4bc1494
clearcasting <lateralcreativity@outlook.com>
2024-09-30 15:59:12 -0500

    done

--- Section 12 Generics/Generics/src/dev/lpa/Main.java
+++ Section 12 Generics/Generics/src/dev/lpa/Main.java
@@ -1,13 +1,33 @@
 package dev.lpa;
 
-record BaseballPlayer(String name, String position) {
+interface Player {
+    String name();
+}
+
+record BaseballPlayer(String name, String position) implements Player {
+
+}
+
+record FootballPlayer(String name, String position) implements Player {
+
+}
+record VolleyballPlayer(String name, String position) implements Player {
 
 }
 
 public class Main {
     public static void main(String[] args) {
-        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
-        BaseballTeam astros = new BaseballTeam("Houston Astros");
+        Affiliation philly = new Affiliation("city", "Philadelphia, PA", "US");
+        BaseballTeam phillies1 = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros1 = new BaseballTeam("Houston Astros");
+        scoreResult(phillies1, 3, astros1, 5);
+
+        SportsTeam phillies2 = new SportsTeam("Philadelphia Phillies");
+        SportsTeam astros2 = new SportsTeam("Houston Astros");
+        scoreResult(phillies2, 3, astros2, 5);
+
+        Team<BaseballPlayer, Affiliation> phillies = new Team<>("Philadelphia Phillies", philly);
+        Team<BaseballPlayer, Affiliation> astros = new Team<>("Houston Astros");
         scoreResult(phillies, 3, astros, 5);
 
         BaseballPlayer harper = new BaseballPlayer("B Harper", "Right Fielder");
@@ -15,11 +35,42 @@ public class Main {
 
         phillies.addTeamMember(harper);
         phillies.addTeamMember(marsh);
+        BaseballPlayer guthrie = new BaseballPlayer("D Guthrie, ", "Center Fielder");
+        phillies.addTeamMember(guthrie);
         phillies.listTeamMembers();
+
+        SportsTeam afc1 = new SportsTeam("Adelaide Crows");
+        Team<FootballPlayer, String> afc = new Team<>("Adelaide Crows", "City of Adelaide, South Australia, in AU");
+        FootballPlayer tex = new FootballPlayer("Tex Walker", "Centre half forward");
+        afc.addTeamMember(tex);
+        FootballPlayer rory = new FootballPlayer("Rory Laird", "Midfield");
+        afc.addTeamMember(rory);
+        afc.listTeamMembers();
+
+        Team<VolleyballPlayer, Affiliation> adelaide = new Team<>("Adelaide Storm");
+        adelaide.addTeamMember(new VolleyballPlayer( "N Roberts", "Setter"));
+        adelaide.listTeamMembers();
+
+        Team<VolleyballPlayer, Affiliation> canberra = new Team<>("Canberra Heat");
+        canberra.addTeamMember(new VolleyballPlayer( "B Black", "Opposite"));
+        canberra.listTeamMembers();
+        scoreResult(canberra, 0, adelaide, 1);
     }
 
     public static void scoreResult(BaseballTeam team1, int t1_score,
                                    BaseballTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+    public static void scoreResult(SportsTeam team1, int t1_score,
+                                   SportsTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+    public static void scoreResult(Team team1, int t1_score,
+                                    Team team2, int t2_score) {
         String message = team1.setScore(t1_score, t2_score);
         team2.setScore(t2_score, t1_score);
         System.out.printf("%s %s %s %n", team1, message, team2);
