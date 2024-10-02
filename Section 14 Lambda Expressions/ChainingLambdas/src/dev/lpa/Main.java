77a06ee7c48c8bd556c3d7bd593e4540352bd0d9
clearcasting <lateralcreativity@outlook.com>
2024-10-02 09:38:10 -0500

    .

---
+++ Section 14 Lambda Expressions/ChainingLambdas/src/dev/lpa/Main.java
@@ -0,0 +1,24 @@
+package dev.lpa;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Tim";
+        Function<String, String> uCase = String::toUpperCase;
+        System.out.println(uCase.apply(name));
+
+        Function<String, String> lastName = s -> s.concat(" Buchalka");
+        Function<String, String> uCaseLastName = uCase.andThen(lastName);
+        System.out.println(uCaseLastName.apply(name));
+
+        uCaseLastName = uCase.compose(lastName);
+        System.out.println(uCaseLastName.apply(name));
+
+        Function<String, String[]> f0 = uCase
+                .andThen(s -> s.concat(" Buchalka"))
+                .andThen(s -> s.split(" "));
+        System.out.println(Arrays.toString(f0.apply(name)));
+    }
+}
